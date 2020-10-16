869d0b0d4f3d1e57a7fc079206d8b79c1dbab9c0
Ashen Kavinda <49136460+Ashenk97@users.noreply.github.com>
2020-10-16 20:32:55 +0530

    updated

--- lib/class/ForeignUser.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ticket_booking_client/class/User.dart';
-
-class ForeignUser {
-  final String passportId;
-  final String country;
-  final User userDetails;
-
-  ForeignUser({this.passportId, this.country, this.userDetails});
-}
--- lib/class/LocalUser.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ticket_booking_client/class/User.dart';
-
-class LocalUser {
-  final String nic;
-  final User userDetail;
-
-  LocalUser({this.nic, this.userDetail});
-}
