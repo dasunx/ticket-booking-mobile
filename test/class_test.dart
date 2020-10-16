0e2447afe1c25bc9bd2a94ca8afa9420ec9aeb67
dasunx <mwdasun@gmail.com>
2020-10-16 20:55:28 +0530

    final touches

---
+++ test/class_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('.split() splits the string on the delimiter', () {
+    expect('foo,bar,baz',
+        allOf([contains('foo'), isNot(startsWith('bar')), endsWith('baz')]));
+  });
+}
